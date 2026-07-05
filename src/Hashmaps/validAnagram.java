276c2303bc748df02286630d2a6bc470d9632a7e
Shivani Chaugule <shivani.chaugule18@gmail.com>
2026-07-05 23:43:59 +0530

    Hashmap question added

--- src/Hashmaps/validAnagram.java
+++ src/Hashmaps/validAnagram.java
@@ -5,24 +5,39 @@ import java.util.HashMap;
 public class validAnagram {
     public static void main(String[] args) {
 
-            HashMap<String,Integer> smap = new HashMap<>();
-            for(int i=1; i<s.length(); i++){
-                char ch = s.charAt(i);
-                if(smap.containsKey()){
-                    int freq = smap,get(key);
-                    smap.put(key, freq+1);
-                }
-                else smap.put(key,1);
+        String s = "anagram";
+        String t = "nagaram";
+
+        HashMap<Character, Integer> smap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (smap.containsKey(ch)) {
+                int freq = smap.get(ch);
+                smap.put(ch, freq + 1);
+            } else {
+                smap.put(ch, 1);
             }
-        HashMap<String,Integer> tmap = new HashMap<>();
-        for(int i=1; i<t.length(); i++){
-            char ch = t.charAt(i);
-            if(smap.containsKey()){
-                int freq = tmap,get(key);
-                smap.put(key, freq+1);
-            }
-            else smap.put(key,1)
         }
 
+        HashMap<Character, Integer> tmap = new HashMap<>();
 
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (tmap.containsKey(ch)) {
+                int freq = tmap.get(ch);
+                tmap.put(ch, freq + 1);
+            } else {
+                tmap.put(ch, 1);
+            }
+        }
+
+        if (smap.equals(tmap)) {
+            System.out.println("Valid Anagram");
+        } else {
+            System.out.println("Not a Valid Anagram");
+        }
+    }
 }
